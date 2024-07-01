5ce7d2630dc687e515a0584fa1597f40ca3209db
ArturoEG365 <ArturoEG354>
2024-07-01 12:06:54 -0600

    Responses Caracola

--- Servers/main.py
+++ Servers/main.py
@@ -1,12 +1,28 @@
 from TikTokLive import TikTokLiveClient
 from TikTokLive.events import ConnectEvent, CommentEvent, LikeEvent, SubscribeEvent
 from colorama import Fore, Style, init
+from tkinter import *
+import random
+
+responses = [
+    "Sí.", 
+    "No.", 
+    "Tal vez algún día...", 
+    "No cuentes con ello.",
+    "Definitivamente sí.", 
+    "Pregunta de nuevo más tarde.", 
+    "No lo creo.",
+    "Es posible.", 
+    "Claro que sí.", 
+    "Sin lugar a dudas."
+]
+
 
 # Initialize colorama
 init()
 
 # Create the client
-client: TikTokLiveClient = TikTokLiveClient(unique_id="@vitolin_r")
+client: TikTokLiveClient = TikTokLiveClient(unique_id="@lofilite5")
 
 # Listen to an event with a decorator!
 @client.on(ConnectEvent)
@@ -18,8 +34,12 @@ async def on_connect(event: ConnectEvent):
 async def on_comment(event: CommentEvent) -> None:
     comment = event.comment
     if comment.lower().startswith("caracola"):
+        # Eliminar "caracola" y cualquier espacio en blanco adicional al principio
+        remaining_text = comment[len("caracola"):].strip()
         print("-----------------------------------------")
-        print(Fore.RED + f"{event.user.nickname} -> {comment}" + Style.RESET_ALL)
+        print(Fore.RED + f"{event.user.nickname} pregunta:" + Style.RESET_ALL)
+        print(Fore.BLUE + f"{remaining_text}" + Style.RESET_ALL)
+        print(Fore.BLUE + random.choice(responses) + Style.RESET_ALL)
         print("-----------------------------------------")
     else:
         print(f"{event.user.nickname} -> {comment}")
@@ -36,8 +56,10 @@ async def on_like(event: LikeEvent) -> None:
 # Event to SubscribeEvent
 @client.on(SubscribeEvent)
 async def on_follow(event: SubscribeEvent) -> None:
-    print(f"{event.user.nickname} followed you")
-
+    print("-----------------------------------------")
+    print(Fore.GREEN + f"{event.user.nickname} followed you")
+    print("-----------------------------------------")
 
 if __name__ == '__main__':
     client.run()
+    
